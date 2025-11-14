efeea356532eacf19155e17b4bdd4d298e7e1b0a
Jordan1881 <85114866+Jordan1881@users.noreply.github.com>
2025-11-14 12:34:29 +0200

    final version 1.0

---
+++ stringCahin/src/test/java/stringCahin/stringCahin/AUnitTest.java
@@ -0,0 +1,19 @@
+package stringCahin.stringCahin;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AUnitTest {
+    @Test
+    public void testCWithStub() {
+        // create stub of B
+        BStub stub = new BStub();
+        // create A with the stub
+        A a = new A(stub);
+
+        // call A — לא לסטאב!
+        String result = a.process("");
+
+        // validate the output
+        assertEquals("HX", result);
+    }
+}
---
+++ stringCahin/src/test/java/stringCahin/stringCahin/BUnitTest.java
@@ -0,0 +1,19 @@
+package stringCahin.stringCahin;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BUnitTest {
+    @Test
+    public void testCWithStub() {
+        // create stub of C
+        CStub stub = new CStub();
+        // create B with the stub
+        B b = new B(stub);
+
+        // call B — לא לסטאב!
+        String result = b.process("");
+
+        // validate the output
+        assertEquals("EY", result);
+    }
+}
