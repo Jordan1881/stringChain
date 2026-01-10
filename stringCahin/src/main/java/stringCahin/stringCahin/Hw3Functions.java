64a2b5d75c6d7bcf28ef0d9ec6dc6e0308a6e4f7
Jordan1881 <85114866+Jordan1881@users.noreply.github.com>
2026-01-10 12:48:13 +0200

    Move Hw3Functions to correct package directory structure (src/main/java/stringCahin/stringCahin/)

---
+++ stringCahin/src/main/java/stringCahin/stringCahin/Hw3Functions.java
@@ -0,0 +1,31 @@
+package stringCahin.stringCahin;
+
+public class Hw3Functions {
+
+    public static int func1(int x, int y, int z) {
+        int res = 0;
+
+        if ((x != 0) & (y == 0)) {
+            res = z / 10;
+        }
+
+        res = res / x;
+        return res;
+    }
+
+    public static int func2(int p, int q) {
+        int x = 0;
+
+        if (p > 0) {
+            x = q / p;
+        }
+
+        return x;
+    }
+
+    public static int func3() {
+        int result = 10;
+        result = result / 0;
+        return result;
+    }
+}
